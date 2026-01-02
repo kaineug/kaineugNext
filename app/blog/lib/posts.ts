6db0902a62239b66eda7e9cb3fb6d12acfe588c2
kaineug <squid-sage-yippee@duck.com>
2026-01-02 10:20:35 +0100

    Started blog page

---
+++ app/blog/lib/posts.ts
@@ -0,0 +1,4 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,14 @@
+import BlogpostPreview from "../Components/articlePreview";
+
 export default function Home() {
   return (
-      <div>
-        <h1>blog</h1>
-        <p>Atkinson lorem ipsum lorem ipsum ipsum yeah yeah ipsum lorem</p>
-        <p>sdjh</p>
-        <p>hd</p>
-      </div>
+        <div>
+            <h1>blog</h1>
+            <p>Atkinson lorem ipsum lorem ipsum ipsum yeah yeah ipsum lorem</p>
+            <p>sdjh</p>
+            <p>hd</p>
+
+            <BlogpostPreview />
+        </div>
   );
 }
